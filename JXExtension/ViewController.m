2803cbb4d7e9ab3f3ba10a864e2f62a6c5bf6d46
Jeason <iJeasonLee@gmail.com>
2017-06-23 17:10:22 +0800

    Initial Commit

---
+++ JXExtension/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JXExtension
+//
+//  Created by Jeason on 2017/6/23.
+//  Copyright © 2017年 Jeason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ JXExtension/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JXExtension
+//
+//  Created by Jeason on 2017/6/23.
+//  Copyright © 2017年 Jeason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
