9d74f252e3337b75af71b06f56836d5652a96ee9
Jeason <jeason.l@qq.com>
2018-05-08 10:54:25 +0800

    [修改]代码细节;

--- JXExtension/Transition/JXInteractiveTransition.h
+++ JXExtension/Transition/JXInteractiveTransition.h
@@ -7,38 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
-
-typedef void(^JXInteractiveGestureConfigBlock)();
-
-//手势的方向
-typedef NS_ENUM(NSUInteger, JXInteractiveGestureDirection) {
-    JXInteractiveGestureDirectionNone = 0,
-    JXInteractiveGestureDirectionLeft,
-    JXInteractiveGestureDirectionRight,
-    JXInteractiveGestureDirectionTop,
-    JXInteractiveGestureDirectionBottom
-};
+#import "JXTransitionConstant.h"
 
 //手势控制哪种转场
 typedef NS_ENUM(NSUInteger, JXInteractiveType) {
     JXInteractiveTypePresent = 0,
     JXInteractiveTypeDismiss,
-    JXInteractiveTypePush,
-    JXInteractiveTypePop,
 };
+
+typedef void(^JXInteractiveGestureDirectionBlock)(JXAnimatorDirection direction);
 
 @interface JXInteractiveTransition : UIPercentDrivenInteractiveTransition
 
-/**记录是否开始手势，判断pop操作是手势触发还是返回键触发*/
-@property (nonatomic, assign, readonly) BOOL isInteractive;
-/**促发手势present的时候的config，config中初始化并present需要弹出的控制器*/
-@property (nonatomic, copy) JXInteractiveGestureConfigBlock presentConfigBlock;
-/**促发手势push的时候的config，config中初始化并push需要弹出的控制器*/
-@property (nonatomic, copy) JXInteractiveGestureConfigBlock pushConfigBlock;
+@property (nonatomic, assign, readonly) BOOL isInteractive; //记录是否开始手势
+@property (nonatomic, assign) CGFloat minPersent; //转场需要的最小百分比, 默认0.3
+@property (nonatomic, copy) JXInteractiveGestureDirectionBlock presentConfigBlock;
 
-/**初始化*/
-- (instancetype)initWithType:(JXInteractiveType)type direction:(JXInteractiveGestureDirection)direction;
-/**给传入的控制器添加手势*/
+- (instancetype)initWithType:(JXInteractiveType)type;
 - (void)addPanGestureForViewController:(UIViewController *)viewController;
 
 @end
--- JXExtension/Transition/JXInteractiveTransition.m
+++ JXExtension/Transition/JXInteractiveTransition.m
@@ -12,36 +12,23 @@
 
 @property (nonatomic, assign, readwrite) BOOL isInteractive;
 @property (nonatomic, weak) UIViewController *viewController;
-@property (nonatomic, assign) JXInteractiveGestureDirection direction;
+@property (nonatomic, assign) JXAnimatorDirection direction;
 @property (nonatomic, assign) JXInteractiveType type;
 
 @end
 
 @implementation JXInteractiveTransition
 
-//- (instancetype)initWithViewController:(UIViewController *)viewController {
-//    if (self = [super init]) {
-//        _isInteractive = NO;
-//        _viewController = viewController;
-//        UIPanGestureRecognizer *panGestureRecognizer =[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureAction:)];
-//        [viewController.view addGestureRecognizer:panGestureRecognizer];
-//    }
-//    return self;
-//}
-
-- (instancetype)initWithType:(JXInteractiveType)type direction:(JXInteractiveGestureDirection)direction {
+- (instancetype)initWithType:(JXInteractiveType)type {
     self = [super init];
     if (self) {
-        _direction = direction;
         _type = type;
+        _minPersent = 0.3;
     }
     return self;
 }
 
 - (void)addPanGestureForViewController:(UIViewController *)viewController {
-    if (self.direction == JXInteractiveGestureDirectionNone) {
-        return;
-    }
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGestureAction:)];
     self.viewController = viewController;
     [viewController.view addGestureRecognizer:pan];
@@ -49,52 +36,55 @@
 
 - (void)panGestureAction:(UIPanGestureRecognizer *)panGesture {
     CGFloat persent = 0;
-    switch (self.direction) {
-        case JXInteractiveGestureDirectionLeft:{
-            CGFloat transitionX = -[panGesture translationInView:panGesture.view].x;
-            persent = transitionX / panGesture.view.frame.size.width;
-        } break;
-        case JXInteractiveGestureDirectionRight:{
-            CGFloat transitionX = [panGesture translationInView:panGesture.view].x;
-            persent = transitionX / panGesture.view.frame.size.width;
-        } break;
-        case JXInteractiveGestureDirectionTop:{
-            CGFloat transitionY = -[panGesture translationInView:panGesture.view].y;
-            persent = transitionY / panGesture.view.frame.size.width;
-        } break;
-        case JXInteractiveGestureDirectionBottom:{
-            CGFloat transitionY = [panGesture translationInView:panGesture.view].y;
-            persent = transitionY / panGesture.view.frame.size.width;
-        } break;
-        default: break;
+    JXAnimatorDirection direction = JXAnimatorDirectionLeft;
+    CGPoint translation = [panGesture translationInView:panGesture.view];
+    CGSize viewSize = panGesture.view.frame.size;
+    CGFloat absX = fabs(translation.x);
+    CGFloat absY = fabs(translation.y);
+    if (MAX(absX, absY) < 10) { // 设置滑动有效距离
+        return;
+    }
+    if (absX > absY) { //水平方向
+        if (translation.x < 0) { //向左滑动
+            direction = JXAnimatorDirectionLeft;
+            persent = -translation.x / viewSize.width;
+        } else { //向右滑动
+            direction = JXAnimatorDirectionRight;
+            persent = translation.x / viewSize.width;
+        }
+    } else { //垂直方向
+        if (translation.y < 0) { //向上滑动
+            direction = JXAnimatorDirectionTop;
+            persent = -translation.y / viewSize.width;
+        } else { //向下滑动
+            direction = JXAnimatorDirectionBottom;
+            persent = translation.y / viewSize.width;
+        }
     }
     switch (panGesture.state) {
         case UIGestureRecognizerStateBegan:{
-            //手势开始的时候标记手势状态，并开始相应的事件
-            self.isInteractive = YES;
-            switch (self.type) {
-                case JXInteractiveTypePresent:{
-                    self.presentConfigBlock ? self.presentConfigBlock() : nil;
-                } break;
-                case JXInteractiveTypeDismiss:{
-                    [self.viewController dismissViewControllerAnimated:YES completion:nil];
-                } break;
-                case JXInteractiveTypePush:{
-                    self.pushConfigBlock ? self.pushConfigBlock() : nil;
-                } break;
-                case JXInteractiveTypePop: {
-                    [self.viewController.navigationController popViewControllerAnimated:YES];
-                } break;
-            }
+
         } break;
         case UIGestureRecognizerStateChanged:{
-            //手势过程中，通过updateInteractiveTransition设置pop过程进行的百分比
+            //手势开始的时候标记手势状态，并开始相应的事件
+            if (!self.isInteractive) {
+                self.isInteractive = YES;
+                switch (self.type) {
+                    case JXInteractiveTypePresent:{
+                        self.presentConfigBlock ? self.presentConfigBlock(direction) : nil;
+                    } break;
+                    case JXInteractiveTypeDismiss:{
+                        [self.viewController dismissViewControllerAnimated:YES completion:nil];
+                    } break;
+                }
+            }
+            //手势过程中，通过updateInteractiveTransition设置返回过程进行的百分比
             [self updateInteractiveTransition:persent];
         } break;
         case UIGestureRecognizerStateEnded:{
             //手势完成后结束标记并且判断移动距离是否过半，过则finishInteractiveTransition完成转场操作，否者取消转场操作
             self.isInteractive = NO;
-            if (persent > 0.3) {
+            if (persent > self.minPersent) {
                 [self finishInteractiveTransition];
             }else{
                 [self cancelInteractiveTransition];
@@ -102,23 +92,6 @@
         } break;
         default: break;
     }
-    //    CGFloat progress = [recognizer translationInView:self.viewController.view].x / (self.viewController.view.bounds.size.width * 1.0);
-    //    progress = MIN(1.0, MAX(0.0, progress));
-    //    self.isInteractive = YES;
-    //    if (recognizer.state == UIGestureRecognizerStateBegan) {
-    //        [self.viewController dismissViewControllerAnimated:YES completion:nil];
-    //    } else if (recognizer.state == UIGestureRecognizerStateChanged) {
-    //        [self updateInteractiveTransition:progress];
-    //    } else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {
-    //        if (progress > 0.3) {
-    //            [self finishInteractiveTransition];
-    //        } else {
-    //            [self cancelInteractiveTransition];
-    //        }
-    //        self.isInteractive = NO;
-    //    }
-    //}
-
 }
 
 @end
